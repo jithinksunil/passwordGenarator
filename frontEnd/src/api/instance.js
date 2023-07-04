994d94b7aa147f369c860bc94c73bceef36f9ea9
jithin k sunil <jithinksunil96@gmail.com>
2023-07-04 20:28:28 +0530

    completed

--- frontEnd/src/api/instance.js
+++ frontEnd/src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance=axios.create({
-    baseURL:'http://localhost:4000'
+    baseURL:'https://passwordgenaratornew.onrender.com'
 })
 axiosInstance.interceptors.request.use((request)=>{
     const accessToken=sessionStorage.getItem('accessToken')
